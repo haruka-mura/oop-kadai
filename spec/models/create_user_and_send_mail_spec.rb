33733cafc2eac5f5ef1ade47d28a721adc544d68
haruka-mura <muramoto.haruka@mwed.co.jp>
2018-08-07 15:05:33 +0900

    Add spec test of CreateUserAndSendMail

--- spec/models/create_user_and_send_mail_spec.rb
+++ spec/models/create_user_and_send_mail_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe CreateUserAndSendMail, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#save' do
+    subject {CreateUserAndSendMail.new(user).save}
+    let(:user) { build(:user) }
+
+    context 'saveが成功するとき' do
+      it { is_expected.to be_truthy }
+    end
+
+    context 'レコードが１増える' do
+      it { expect{subject}.to change{User.count}.by(1) }
+    end
+  end
 end
